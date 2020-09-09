3b163a2e536c3afaf933039a7fdacbba656cc842
mayank malhotra <mayank31m@gmail.com>
2020-09-09 16:51:07 +0530

    Updates

---
+++ precache-manifest.6787856e49bbf53e462be0858ef3976e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10bf0369184be19e4c0214924c01270",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "1cc3eda6d2671769b977",
+    "url": "/myapp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7df51bf27a005c896c89",
+    "url": "/myapp/static/css/main.4e1f7464.chunk.css"
+  },
+  {
+    "revision": "1cc3eda6d2671769b977",
+    "url": "/myapp/static/js/2.5fd00f00.chunk.js"
+  },
+  {
+    "revision": "028b2c62d6c0836a1b02edbfbd3af65e",
+    "url": "/myapp/static/js/2.5fd00f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df51bf27a005c896c89",
+    "url": "/myapp/static/js/main.a7521c29.chunk.js"
+  },
+  {
+    "revision": "36c9810107d8e51c5726",
+    "url": "/myapp/static/js/runtime-main.1ba3c07f.js"
+  },
+  {
+    "revision": "69c26b88454531967308be1d1d991279",
+    "url": "/myapp/static/media/3.69c26b88.jpeg"
+  },
+  {
+    "revision": "82b92dd83e991cd60b4a14ffb5be8cba",
+    "url": "/myapp/static/media/house.82b92dd8.jpg"
+  },
+  {
+    "revision": "95bf7f673f22da70f2e71fbe5d9b382b",
+    "url": "/myapp/static/media/house3.95bf7f67.jpg"
+  }
+]);
